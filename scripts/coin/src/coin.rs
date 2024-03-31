9102a77f9a16f5ec15ff7472781562891cee6876
Ramses <ram.hdzven@gmail.com>
2024-03-30 18:27:47 -0600

    coin collection working

--- scripts/coin/src/coin.rs
+++ scripts/coin/src/coin.rs
@@ -3,18 +3,23 @@ use gdnative::api::Area2D;
 
 #[derive(NativeClass)]
 #[inherit(Area2D)]
+#[register_with(Self::register_signals)]
 pub struct Coin;
 
 #[methods]
 impl Coin {
+    fn register_signals(builder: &ClassBuilder<Self>) {
+        builder.signal("coin_collected").done();
+    }
+
     fn new(_base: &Area2D)-> Self {
-        _base.emit_signal("coin_collected", &[]); 
         Coin
     }
 
     #[method] 
     #[allow(nonstandard_style)]
     fn _on_Coin2D_body_entered(&self, #[base] _base: &Area2D, _body: Option<Ref<KinematicBody2D>>) {
+        _base.emit_signal("coin_collected", &[]); 
         _base.queue_free();
     }
 }
